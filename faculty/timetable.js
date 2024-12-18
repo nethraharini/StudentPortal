7e333a515fda6612e1f02ed966e2507022ce90de
nethraharini <nethraharini0904@gmail.com>
2024-12-18 16:27:05 +0530

    new changes from nethra

---
+++ faculty/timetable.js
@@ -0,0 +1,130 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+    }
+]
+const Monday =[
+    {   
+        time: '9:00 AM to 9:55 AM',
+        roomNumber: 'IT 318',
+        subject: 'Maths',
+    },
+    {   
+        time: '11:05 AM to 12:00 PM',
+        roomNumber: 'IT 318',
+        subject: 'Java Lab',
+    },
+    {   
+        time: '2:00 PM to 2:55 PM',
+        roomNumber: 'IT 320',
+        subject: 'DSA',
+    },
+    {
+        time:'2:55 PM to 4:45 PM',
+        roomNumber:'IT 320',
+        subject:'Library'
+    }
+]
+const Tuesday =[
+    {   
+        time: '9:00 AM to 9:55 AM',
+        roomNumber: 'IT 318',
+        subject: 'OS',
+    },
+    {   
+        time: '11:05 AM to 12:00 PM',
+        roomNumber: 'IT 318',
+        subject: 'DBMS',
+    },
+    {   
+        time: '2:00 PM to 2:55 PM',
+        roomNumber: 'IT 320',
+        subject: 'DSA Lab',
+    },
+    {
+        time:'2:55 PM to 4:45 PM',
+        roomNumber:'IT 320',
+        subject:'DSA Lab'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '9:00 AM to 9:55 AM',
+        roomNumber: 'IT 318',
+        subject: 'DBMS',
+    },
+    {   
+        time: '11:05 AM to 12:00 PM',
+        roomNumber: 'IT 318',
+        subject: 'OS',
+    },
+    {   
+        time: '2:00 PM to 2:55 PM',
+        roomNumber: 'IT 320',
+        subject: 'Communication skills',
+    },
+    {
+        time:'2:55 PM to 4:45 PM',
+        roomNumber:'IT 320',
+        subject:'Association hour'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '9:00 AM to 9:55 AM',
+        roomNumber: 'IT 318',
+        subject: 'Python Lab',
+    },
+    {   
+        time: '11:05 AM to 12:00 PM',
+        roomNumber: 'IT 318',
+        subject: 'Python Lab',
+    },
+    {   
+        time: '2:00 PM to 2:55 PM',
+        roomNumber: 'IT 320',
+        subject: 'DBMS',
+    },
+    {
+        time:'2:55 PM to 4:45 PM',
+        roomNumber:'IT 320',
+        subject:'Maths'
+    }
+]
+
+const Friday =[
+    {   
+        time: '9:00 AM to 9:55 AM',
+        roomNumber: 'IT 318',
+        subject: 'Java',
+    },
+    {   
+        time: '11:05 AM to 12:00 PM',
+        roomNumber: 'IT 318',
+        subject: 'Java',
+    },
+    {   
+        time: '2:00 PM to 2:55 PM',
+        roomNumber: 'IT 320',
+        subject: 'Library',
+    },
+    {
+        time:'2:55 PM to 4:45 PM',
+        roomNumber:'IT 320',
+        subject:'DBMS Lab'
+    }
+
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+    }
+    
+]
